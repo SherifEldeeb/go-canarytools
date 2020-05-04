87a6ee2f21a3c795bed4b9826b18b533e5d97211
Sherif Eldeeb <archeldeeb@gmail.com>
2020-05-05 02:35:40 +0300

    remove mappings

--- response.go
+++
@@ -1,9 +0,0 @@
-package canarytools
-
-// PingResponse holds the ping API response.
-// if the result is "success", Message will be empty,
-// otherwise it will contain failure reason.
-type PingResponse struct {
-	Message string `json:"message,omitempty"`
-	Result  string `json:"result,omitempty"`
-}
