e993c6abc43a2c9457fea467119544071432e69d
Sherif Eldeeb <archeldeeb@gmail.com>
2020-05-22 15:41:27 +0300

    check

--- ChirpForwarder.go
+++ ChirpForwarder.go
@@ -7,12 +7,48 @@ import log "github.com/sirupsen/logrus"
 type ChirpForwarder struct {
 	// configs
 	cfg ChirpForwarderConfig
+
+	// Work chans
+	incidentsChan         chan Incident
+	filteredIncidentsChan chan Incident
+	outChan               chan []byte
+	incidentAckerChan     chan []byte
+
 	// interfaces
 	feeder        Feeder
 	incidentAcker IncidentAcker
 	filter        Filter
 	mapper        Mapper
 	forwarder     Forwarder
+
 	// logger
 	l *log.Logger
+}
+
+// NewChirpForwarder creates a new chirp forwarder
+func NewChirpForwarder(cfg ChirpForwarderConfig) (cf *ChirpForwarder, err error) {
+	cf = &ChirpForwarder{}
+
+	cf.cfg = cfg
+
+	// create work chans
+	cf.incidentsChan = make(chan Incident)
+	cf.filteredIncidentsChan = make(chan Incident)
+	cf.outChan = make(chan []byte)
+	cf.incidentAckerChan = make(chan []byte)
+
+	// set logger
+	cf.l = log.New()
+	return
+}
+
+// Run starts forwarding incidents
+func (cf *ChirpForwarder) Run() {
+
+	// All good, let's roll...
+	go cf.feeder.Feed(cf.incidentsChan)
+	go cf.incidentAcker.AckIncidents(cf.incidentAckerChan)
+	go cf.filter.Filter(cf.incidentsChan, cf.filteredIncidentsChan)
+	go cf.mapper.Map(cf.filteredIncidentsChan, cf.outChan)
+	cf.forwarder.Forward(cf.outChan, cf.incidentAckerChan)
 }
