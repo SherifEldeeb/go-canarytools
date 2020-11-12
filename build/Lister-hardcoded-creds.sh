d925e7377c8ca5b8697564fd62c4135158bac4a3
Sherif Eldeeb <sherif@thinkst.com>
2020-11-12 13:41:42 +0200

    check

---
+++ build/Lister-hardcoded-creds.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+
+if [ "$#" -ne 3 ]; then
+    echo "Illegal number of parameters $#"
+    exit 2
+fi
+
+BUILDTIME=$(date '+%Y-%m-%dT%H:%M:%S')
+SHA1VER=$(git rev-parse HEAD)
+TOOL="TokenLister"
+
+# build the binaries
+pushd ../cmd/$TOOL
+GOOS=darwin go build -v -ldflags "-X main.DOMAIN=$1  -X main.APIKEY=$2 -X main.BUILDTIME=$BUILDTIME -X main.SHA1VER=$SHA1VER -w -s -linkmode=internal" -o ../../build/$3/$TOOL-macos/$TOOL
+GOOS=linux go build -v -ldflags "-X main.DOMAIN=$1  -X main.APIKEY=$2  -X main.BUILDTIME=$BUILDTIME -X main.SHA1VER=$SHA1VER -w -s -linkmode=internal" -o ../../build/$3/$TOOL-linux/$TOOL
+GOOS=windows go build -v -ldflags "-X main.DOMAIN=$1  -X main.APIKEY=$2  -X main.BUILDTIME=$BUILDTIME -X main.SHA1VER=$SHA1VER -w -s -linkmode=internal" -o ../../build/$3/$TOOL-windows/$TOOL.exe
+popd
