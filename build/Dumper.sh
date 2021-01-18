51c338dc15dc2c9f4ce5c4960f8fbe189da8c168
Sherif Eldeeb <sherif@thinkst.com>
2021-01-18 09:04:34 +0200

    dumper bash

---
+++ build/Dumper.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+
+if [ "$#" -ne 3 ]; then
+    echo "Illegal number of parameters $#"
+    exit 2
+fi
+
+BUILDTIME=$(date '+%Y-%m-%dT%H:%M:%S')
+SHA1VER=$(git rev-parse HEAD)
+TOOL="CanaryDeleter"
+
+# build the binaries
+pushd ../cmd/$TOOL
+GOOS=darwin go build -v -ldflags "-X main.DOMAIN=$1  -X main.APIKEY=$2 -X main.BUILDTIME=$BUILDTIME -X main.SHA1VER=$SHA1VER -w -s -linkmode=internal" -o ../../build/$3/$TOOL-macos/CanaryDumper
+GOOS=linux go build -v -ldflags "-X main.DOMAIN=$1  -X main.APIKEY=$2  -X main.BUILDTIME=$BUILDTIME -X main.SHA1VER=$SHA1VER -w -s -linkmode=internal" -o ../../build/$3/$TOOL-linux/CanaryDumper
+GOOS=windows go build -v -ldflags "-X main.DOMAIN=$1  -X main.APIKEY=$2  -X main.BUILDTIME=$BUILDTIME -X main.SHA1VER=$SHA1VER -w -s -linkmode=internal" -o ../../build/$3/$TOOL-windows/CanaryDumper.exe
+popd
