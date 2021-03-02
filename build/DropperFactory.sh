f314052466bb85b0301c053fe65ec203974e0b43
Sherif Eldeeb <sherif@thinkst.com>
2021-03-02 10:07:55 +0200

    Add DropperFactory.sh

---
+++ build/DropperFactory.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -ex
+
+if [ "$#" -ne 4 ]; then
+    echo "Illegal number of parameters $#, must be 4"
+    exit 2
+fi
+
+BUILDTIME=$(date '+%Y-%m-%dT%H:%M:%S')
+SHA1VER=$(git rev-parse HEAD)
+TOOL="TokenDropper"
+
+pushd ../cmd/$TOOL
+# build the binaries
+# macos
+GOOS=darwin go build -v -ldflags "-X main.DOMAIN=$1  -X main.FACTORYAUTH=$2 -X main.FLOCKID=$3  -X main.BUILDTIME=$BUILDTIME -X main.SHA1VER=$SHA1VER -w -s -linkmode=internal" -o ../../build/$3/$TOOL-macos/$TOOL
+pushd ../../build/$3/$TOOL-macos/
+zip -r -9 $TOOL.zip $TOOL && rm $TOOL
+popd
+
+# linux
+GOOS=linux go build -v -ldflags "-X main.DOMAIN=$1  -X main.FACTORYAUTH=$2 -X main.FLOCKID=$3  -X main.BUILDTIME=$BUILDTIME -X main.SHA1VER=$SHA1VER -w -s -linkmode=internal" -o ../../build/$3/$TOOL-linux/$TOOL
+pushd ../../build/$3/$TOOL-linux/
+zip -r -9 $TOOL.zip $TOOL && rm $TOOL
+popd
+
+# windows
+GOOS=windows go build -v -ldflags "-X main.DOMAIN=$1  -X main.FACTORYAUTH=$2 -X main.FLOCKID=$3  -X main.BUILDTIME=$BUILDTIME -X main.SHA1VER=$SHA1VER -w -s -linkmode=internal" -o ../../build/$3/$TOOL-windows/$TOOL.exe
+pushd ../../build/$3/$TOOL-windows/
+zip -r -9 $TOOL.zip $TOOL.exe && rm $TOOL.exe
+popd
+
+popd
+
+zip -r -9 "$3-$(date '+%Y-%m-%dT%H_%M_%S').zip" $3
